ba7388a3ac910725da7d0a3305b16b5dd1b30414
wkx <1326454178@qq.com>
2017-07-07 11:00:17 +0800

    er

---
+++ Srouter.php
@@ -0,0 +1,8 @@
+<?php
+	$hash=$_GET['hash'];
+	switch($hash){
+		case 'first':echo '<h1>这是一</h1>';break;
+		case 'second':echo '<h1>这是er</h1>';break;
+		case 'third':echo '<h1>这是san</h1>';break;
+	}
+?>
